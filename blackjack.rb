87fa08cae8a5ba57e77d52f7b6811336dde00cbf
Parinda Darden <parinda.darden@gmail.com>
2013-11-25 13:52:09 -0500

    first commit

---
+++ blackjack.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+SUITS = ['♠', '♣', '♥', '♦']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+def build_deck
+  deck = []
+
+  SUITS.each do |suit|
+    VALUES.each do |value|
+      deck.push(value + suit)
+    end
+  end
+
+  deck.shuffle
+end
